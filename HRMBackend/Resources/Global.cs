cdc1903db1f84d220607512d5c0c93310a0f56bc
Minh LQ <minhlq@epayjsc.com>
2024-01-24 15:34:20 +0700

    add extensions + const

---
+++ HRMBackend/Resources/Global.cs
@@ -0,0 +1,11 @@
+﻿namespace HRMBackend.Resources
+{
+    public static class Global
+    {
+    }
+
+    public static class Constant 
+    {
+        public const int TimeOutCancelDAO = 5000; // Ms
+    }
+}
